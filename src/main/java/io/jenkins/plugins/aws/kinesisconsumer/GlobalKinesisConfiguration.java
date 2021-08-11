7fef0cb6c89921e40b1dc5b3ab5ae0944d95f19d
Fabio Ponciroli <ponch78@gmail.com>
2021-08-11 12:41:46 +0200

    Settings configuration
    Added consumer gloobal configuration. Configurable parameters are:
    * AWS Region
    * Kinesis Streams
    
    Change-Id: Ibba09e8e7e4297a2f6f991701e042ae5029af5b0

---
+++ src/main/java/io/jenkins/plugins/aws/kinesisconsumer/GlobalKinesisConfiguration.java
@@ -0,0 +1,113 @@
+package io.jenkins.plugins.aws.kinesisconsumer;
+
+import hudson.Extension;
+import hudson.ExtensionList;
+import jenkins.model.GlobalConfiguration;
+import net.sf.json.JSONObject;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.StaplerRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Class representing the Global Kinesis configuration
+ *
+ * @author Fabio Ponciroli
+ */
+
+@Extension
+@Symbol("aws-kinesis-consumer")
+public class GlobalKinesisConfiguration extends GlobalConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalKinesisConfiguration.class);
+    private boolean kinesisConsumerEnabled;
+    private String region;
+    private List <KinesisStreamItem> kinesisStreamItems;
+
+    /**
+     * Set AWS Region when loading the global configuration page
+     * @param regionString
+     *          AWS Region to connect to. This parameter is optional.
+     *          If set it will override any Region set in the Region Provider Chain.
+     */
+    @DataBoundSetter
+    public void setRegion(String regionString) {
+        this.region = regionString;
+    }
+
+    /**
+     * Set the enabled/disabled status of the plugin when loading the global configuration page
+     * @param kinesisConsumerEnabled
+     *          enabled/disabled status
+     */
+    @DataBoundSetter
+    public void setKinesisConsumerEnabled(boolean kinesisConsumerEnabled) {
+        this.kinesisConsumerEnabled = kinesisConsumerEnabled;
+    }
+
+    /**
+     * Set KinesisStreamItem values when loading the global configuration page
+     * @param kinesisStreamItems
+     *          KinesisStreamItem to set
+     */
+    @DataBoundSetter
+    public void setKinesisStreamItems(List <KinesisStreamItem> kinesisStreamItems) {
+        this.kinesisStreamItems = kinesisStreamItems;
+    }
+
+    public GlobalKinesisConfiguration() { load(); }
+
+    /**
+     * Get the GlobalKinesisConfiguration
+     *
+     * @return the global Kinesis configuration
+     */
+    public static GlobalKinesisConfiguration get() {
+        return ExtensionList.lookupSingleton(GlobalKinesisConfiguration.class);
+    }
+
+    /**
+     * Indicates if the plugin is enabled or not
+     *
+     * @return a boolean indicating the status of the plugin
+     */
+    public boolean isKinesisConsumerEnabled() {
+        return kinesisConsumerEnabled;
+    }
+
+    /**
+     * Get the AWS Region to connect to from the configuration page
+     *
+     * @return AWS Region to connect to
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    public List <KinesisStreamItem> getKinesisStreamItems() {
+        return kinesisStreamItems;
+    }
+
+    /**
+     *
+     * @param req
+     *          <code>StaplerRequest</code> submitted when saving the
+     *          configuration page
+     * @param json
+     *          JSON containing the configuration parameters set
+     * @return <code>true<code/> if the configuration was correctly saved,
+     *          <code>false</code> otherwise
+     */
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) {
+        if (kinesisStreamItems != null) {
+            kinesisStreamItems.clear();
+        }
+        req.bindJSON(this, json);
+        save();
+        return true;
+    }
+}
---
+++ src/main/java/io/jenkins/plugins/aws/kinesisconsumer/KinesisStreamItem.java
@@ -0,0 +1,68 @@
+package io.jenkins.plugins.aws.kinesisconsumer;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang3.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Class representing a Stream configuration
+ *
+ * @author Fabio Ponciroli
+ */
+public class KinesisStreamItem implements Describable<KinesisStreamItem> {
+    private String streamName = null;
+
+    /**
+     * Creates KinesisStreamItem instance.
+     *
+     * @param streamName
+     *            the queue name.
+     */
+    @DataBoundConstructor
+    public KinesisStreamItem(String streamName) {
+        this.streamName = StringUtils.stripToNull(streamName);
+    }
+
+    /**
+     * Get the stream name
+     *
+     * @return the stream name
+     */
+    public String getStreamName() {
+        return streamName;
+    }
+
+    /**
+     *  Set the stram name
+     *
+     * @param streamName
+     *          stream name
+     */
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends Descriptor<KinesisStreamItem> {
+        @Override
+        public String getDisplayName() {
+            return "";
+        }
+
+    }
+
+    /**
+     * Get a Descriptor<KinesisStreamItem> instance
+     *
+     * @return a Descriptor<KinesisStreamItem> instance
+     */
+    @Override
+    @NonNull
+    public Descriptor <KinesisStreamItem> getDescriptor() {
+        return (Descriptor<KinesisStreamItem>) Jenkins.get().getDescriptorOrDie(getClass());
+    }
+}
