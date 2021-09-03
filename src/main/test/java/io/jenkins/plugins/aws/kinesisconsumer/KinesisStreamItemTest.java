0300be1dc57c45322b492e419cae6b0cf40c3496
Antonio Barone <syntonyze@gmail.com>
2021-09-03 22:40:15 +0200

    Allow to test stream connectivity
    During configuration, allow to test correct connectivity to each stream.
    
    Bug: JENKINS-66493
    Change-Id: Ie13beb34a58fc73b08365d1ddf68570fb79e0890

---
+++ src/main/test/java/io/jenkins/plugins/aws/kinesisconsumer/KinesisStreamItemTest.java
@@ -0,0 +1,42 @@
+package io.jenkins.plugins.aws.kinesisconsumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.KINESIS;
+
+import hudson.util.FormValidation;
+import org.junit.Before;
+import org.junit.Test;
+import org.jvnet.hudson.test.For;
+
+@For(KinesisStreamItem.class)
+public class KinesisStreamItemTest extends LocalStackTest {
+  private KinesisStreamItem.DescriptorImpl objectUnderTest;
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+    objectUnderTest = new KinesisStreamItem.DescriptorImpl();
+  }
+
+  @Test
+  public void shouldFailWhenStreamDoesNotExist() {
+    FormValidation result =
+        objectUnderTest.doTestConnection(
+            localstack.getRegion(),
+            localstack.getEndpointOverride(KINESIS).toASCIIString(),
+            "notExisting");
+    assertEquals(FormValidation.Kind.ERROR, result.kind);
+  }
+
+  @Test
+  public void shouldSucceedWhenStreamDoesExist() throws InterruptedException {
+    String streamName = "foobar";
+    createStreamAndWait(streamName);
+    FormValidation result =
+        objectUnderTest.doTestConnection(
+            localstack.getRegion(),
+            localstack.getEndpointOverride(KINESIS).toASCIIString(),
+            streamName);
+    assertEquals(FormValidation.Kind.OK, result.kind);
+  }
+}
