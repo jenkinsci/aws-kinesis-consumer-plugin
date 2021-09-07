0120fbd0c28d9e86c68e8ca6dbf13f34c155d7a8
Antonio Barone <syntonyze@gmail.com>
2021-09-07 16:03:58 +0200

    Handle KinesisConsumer shutdown
    Trigger kinesis consumer shutdown upon Jenkins shutdown.
    In order to achieve this, the starting and the stopping of the kinesis
    consumer is delegated to a singleton manager listener which keeps track
    of the running consumer.
    
    Bug: JENKINS-66495
    Change-Id: If446bfa9a12f3913fce9a550d45d2f2f6d31f08d

--- src/test/java/io/jenkins/plugins/aws/kinesisconsumer/BaseLocalStack.java
+++ src/test/java/io/jenkins/plugins/aws/kinesisconsumer/BaseLocalStack.java
@@ -4,7 +4,13 @@ import static org.testcontainers.containers.localstack.LocalStackContainer.Servi
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.KINESIS;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import io.jenkins.plugins.aws.kinesisconsumer.utils.WaitUtil;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -16,6 +22,9 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
 import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.awssdk.services.kinesis.model.CreateStreamRequest;
 import software.amazon.awssdk.services.kinesis.model.DescribeStreamRequest;
@@ -24,10 +33,18 @@ import software.amazon.awssdk.services.kinesis.model.StreamStatus;
 public class BaseLocalStack {
   @Rule public JenkinsRule j = new JenkinsRule();
 
-  private static final Duration STREAM_CREATION_TIMEOUT = Duration.ofSeconds(10);
-  private static final int LOCALSTACK_PORT = 4566;
+  protected static final String STREAM_NAME = "test-stream";
+  protected static final String LEASE_TABLE_NAME = "jenkins-kinesis-consumer-test-stream";
+  protected static final Duration TABLE_CREATION_TIMEOUT = Duration.ofSeconds(60);
+  protected static final Duration STREAM_CREATION_TIMEOUT = Duration.ofSeconds(10);
+  protected static final Duration RECORD_CONSUMED_TIMEOUT = Duration.ofMinutes(5);
+  protected static final int LOCALSTACK_PORT = 4566;
 
+  protected static Injector injector;
   protected KinesisClient kinesisClient;
+  protected DynamoDbAsyncClient dynamoDbAsynClient;
+  protected KinesisConsumerManager kinesisConsumerManager;
+  protected GlobalKinesisConfiguration globalKinesisConfiguration;
 
   @ClassRule
   public static LocalStackContainer localstack =
@@ -35,6 +52,16 @@ public class BaseLocalStack {
           .withServices(DYNAMODB, KINESIS, CLOUDWATCH)
           .withEnv("USE_SSL", "true")
           .withExposedPorts(LOCALSTACK_PORT);
+
+  class TestKinesisConsumerModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+      globalKinesisConfiguration = initGlobalConfig(STREAM_NAME);
+      bind(GlobalKinesisConfiguration.class).toInstance(globalKinesisConfiguration);
+      install(new KinesisConsumerModule());
+    }
+  }
 
   @Before
   public void setUp() throws Exception {
@@ -54,6 +81,11 @@ public class BaseLocalStack {
     System.setProperty("region", localstack.getRegion());
     System.setProperty("aws.accessKeyId", localstack.getAccessKey());
     System.setProperty("aws.secretAccessKey", localstack.getSecretKey());
+
+    globalKinesisConfiguration = initGlobalConfig(STREAM_NAME);
+    injector = Guice.createInjector(new TestKinesisConsumerModule());
+    dynamoDbAsynClient = injector.getInstance(DynamoDbAsyncClient.class);
+    kinesisConsumerManager = injector.getInstance(KinesisConsumerManager.class);
   }
 
   @After
@@ -76,5 +108,37 @@ public class BaseLocalStack {
                 .streamStatus()
                 .equals(StreamStatus.ACTIVE),
         STREAM_CREATION_TIMEOUT);
+  }
+
+  private GlobalKinesisConfiguration initGlobalConfig(String streamName) {
+    GlobalKinesisConfiguration globalKinesisConfiguration = new GlobalKinesisConfiguration();
+    globalKinesisConfiguration.setKinesisConsumerEnabled(true);
+    globalKinesisConfiguration.setLocalEndpoint(
+        localstack.getEndpointOverride(KINESIS).toASCIIString());
+    globalKinesisConfiguration.setRegion(localstack.getRegion());
+    globalKinesisConfiguration.setApplicationName("jenkins-kinesis-consumer");
+    KinesisStreamItem kinesisStreamItem = new KinesisStreamItem(streamName, "TRIM_HORIZON");
+
+    globalKinesisConfiguration.setKinesisStreamItems(Collections.singletonList(kinesisStreamItem));
+
+    globalKinesisConfiguration.load();
+    return globalKinesisConfiguration;
+  }
+
+  protected void waitForLeaseTable() throws InterruptedException {
+
+    WaitUtil.waitUntil(
+        () -> {
+          try {
+            ScanResponse sr =
+                dynamoDbAsynClient
+                    .scan(ScanRequest.builder().tableName(LEASE_TABLE_NAME).build())
+                    .get();
+            return !sr.items().isEmpty();
+          } catch (ExecutionException | InterruptedException ignored) {
+          }
+          return false;
+        },
+        TABLE_CREATION_TIMEOUT);
   }
 }
